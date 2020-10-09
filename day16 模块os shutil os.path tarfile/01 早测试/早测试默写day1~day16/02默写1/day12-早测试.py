80ea366d12eea61891e51ac2b404229d5c756017
cn5036518 <cn5036520@163.com>
2020-10-09 23:03:44 +0800

    day17 2020.10.9

--- day16 模块os shutil os.path tarfile/01 早测试/早测试默写day1~day16/02默写1/day12-早测试.py
+++ day16 模块os shutil os.path tarfile/01 早测试/早测试默写day1~day16/02默写1/day12-早测试.py
@@ -42,18 +42,61 @@ print(wangwen)
 print('--------------------------------4')
 
 # 5.什么是迭代器
-
-
+# 能被next函数调用,并且不断返回下一个值的对象就是迭代器-Iterator
+# 取值到最后,无值可取,就返回stopiteration
 
 # 6.如何定义迭代器
+# it = iter(iterable)或者
+# it = iterable.__iter__()
+
 # 7.如何调用迭代器
+# next(it)  #一次取一个
+
+# next for  #用多少取多少
+# for i in range(3):
+	# next(it)
+
+# for #全部取完
+# for i in it:
+	# print(i)
+
+# list  #强转
+# print(list(it))
+
+
 # 8.如何判断迭代器
-# 9.法代器和可迭代对象的区别
+# from collections import  Iterator
+# isinstance(it,Iterator)
+
+# if __iter__ in dir(it) and __next__ in dir(it)
+
+# 9.迭代器和可迭代对象的区别
+# 迭代器Iterator是可迭代对象Iterable
+# 可迭代对象不一定是迭代器
+
 # 10 .dic = {97:"a",98:'b",99:"e"}
 # 给你一个列表["a","b","c]=> [97,98,991
+
 # 11. ""123 =>123 不使用int的情况下实现该操作;
+
 # 12.filter过滤奇数
+lst = [1,2,3,4,5,6]
+def func(n):
+	if n % 2 == 1:
+		return True
+	else:
+		return False
+	
+it = filter(func,lst)
+print(list(it))  #[1, 3, 5]
+print('----------------------12')
+
 # 13 . sorted 和sort区别
+# sort 只能用于list
+     # 会改变list本身
+# sorted 可以适用于容器
+       # 不会改变list本身,会新建一个list
+
 # 14 .tup = (19,23,42,87）按照和10余数排序
 
 
